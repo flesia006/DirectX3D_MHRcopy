48f77a30b94fc54d338d7f1ce089570ba661c4d4
JH Lee <ljh1642@gmail.com>
2024-01-29 17:59:41 +0900

    UI 샘플 코드 및 텍스처 추가

--- DirectX3D/Scenes/GameScene.cpp
+++ DirectX3D/Scenes/GameScene.cpp
@@ -23,6 +23,53 @@ GameScene::GameScene()
 	//CAM->SetTarget(player);
 	//KunaiManager::Get();
 	/////
+
+	// 내부 데이터에 의해 안변하는 UI들은 생성자에서 UpdateWorld,
+	// 변하는 UI들은 Update 함수에서 UpdateWorld시킴
+	clockFrame = new Quad(L"Textures/UI/ClockFrame.png");
+	clockFrame->Pos() = { 80,640,0 };
+	clockFrame->UpdateWorld();
+
+	durability = new Quad(L"Textures/UI/Durability.png");
+	durability->Pos() = { 200,600,0 };
+	durability->UpdateWorld();
+
+	durability_gauge = new Quad(L"Textures/UI/Durability_Gauge.png");
+	durability_gauge->Pos() = { 200,600,0 };
+
+	hpBar = new Quad(L"Textures/UI/HpBar.png");
+	hpBar->Pos() = { 140,650,0 };
+
+	idBar = new Quad(L"Textures/UI/IDBar.png");
+	idBar->Pos() = { 220,680,0 };
+	idBar->UpdateWorld();
+
+	itemSlot = new Quad(L"Textures/UI/ItemSlot.png");
+	itemSlot->Pos() = { 1160,120,0 };
+	itemSlot->UpdateWorld();
+
+	lsCoting = new Quad(L"Textures/UI/LSCoting.png");
+	lsCoting->Pos() = { 340,600,0 };
+
+	lsCoting2 = new Quad(L"Textures/UI/LSCoting2.png");
+	lsCoting2->Pos() = { 340,600,0 };
+
+	lsGauge = new Quad(L"Textures/UI/LSGauge.png");
+	lsGauge->Pos() = { 340,600,0 };
+	lsGauge->UpdateWorld();
+
+	lsGauge2 = new Quad(L"Textures/UI/LSGauge2.png");
+	lsGauge2->Pos() = { 347,600,0 };
+
+	quickSlot = new Quad(L"Textures/UI/QuickSlot.png");
+	quickSlot->Pos() = { 1000,120,0 };
+	quickSlot->UpdateWorld();
+
+	slingerBug = new Quad(L"Textures/UI/SlingerBug.png");
+	slingerBug->Pos() = { 640,120,0 };
+
+	staminarBar = new Quad(L"Textures/UI/StaminaBar.png");
+	staminarBar->Pos() = { 140,630,0 };
 }
 
 GameScene::~GameScene()
@@ -31,6 +78,20 @@ GameScene::~GameScene()
 
 	//RobotManager::Delete();
 	//KunaiManager::Delete();
+
+	delete clockFrame;
+	delete durability;
+	delete durability_gauge;
+	delete hpBar;
+	delete idBar;
+	delete itemSlot;
+	delete lsCoting;
+	delete lsCoting2;
+	delete lsGauge;
+	delete lsGauge2;
+	delete quickSlot;
+	delete slingerBug;
+	delete staminarBar;
 }
 
 void GameScene::Update()
@@ -41,6 +102,14 @@ void GameScene::Update()
 //	boss->Update();
 	//
 	player->Update();
+
+	durability_gauge->UpdateWorld();
+	hpBar->UpdateWorld();
+	lsCoting->UpdateWorld();
+	lsCoting2->UpdateWorld();
+	lsGauge2->UpdateWorld();
+	slingerBug->UpdateWorld();
+	staminarBar->UpdateWorld();
 }
 
 void GameScene::PreRender()
@@ -63,6 +132,20 @@ void GameScene::PostRender()
 {
 	//	naruto->PostRender();
 	player->PostRender();
+
+	clockFrame->Render();
+	durability->Render();
+	durability_gauge->Render();
+	hpBar->Render();
+	idBar->Render();
+	itemSlot->Render();
+	lsCoting->Render();
+	lsCoting2->Render();
+	lsGauge->Render();
+	lsGauge2->Render();
+	quickSlot->Render();
+	slingerBug->Render();
+	staminarBar->Render();
 }
 
 void GameScene::GUIRender()
