f83995ad0868d823d11a3674535bc85642240b21
원영 최 <wyoung0502@gmail.com>
2024-01-31 00:13:41 +0900

    아아아...

--- DirectX3D/Scenes/GameScene.h
+++ DirectX3D/Scenes/GameScene.h
@@ -22,12 +22,10 @@ private:
     Model* ground = nullptr;
     SkyBox* sky = nullptr;
 
-<<<<<<< HEAD
     Shadow* shadow;
     LightBuffer::Light* light;
 
     BlendState* blendState[2];
-=======
     Quad* clockFrame;
     Quad* durability;
     Quad* durability_gauge;
@@ -41,6 +39,5 @@ private:
     Quad* quickSlot;
     Quad* slingerBug;
     Quad* staminarBar;
->>>>>>> 3c1ba98ac6e9df4b73434fdac1515fec3796e4db
 };
 
